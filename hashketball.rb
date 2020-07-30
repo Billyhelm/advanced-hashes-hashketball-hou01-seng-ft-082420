3faee124bbfba8380ae93853e05572893ce5a24b
Billy Helm <billyhelm88@gmail.com>
2020-07-30 16:07:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,7 +200,7 @@ def most_points_scored
   game_hash.each do |(k,v), place|
     place[:players].each do |players|
       mvp = players[:points].max
-      binding.pry
+      #binding.pry
     end 
   end 
   p mvp
